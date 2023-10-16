56db83a5970558eabe4c17c243579bedd51f3fcb
Lucas Fernandes <lucasmatdlx@hotmail.com>
2023-10-16 19:20:30 -0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,50 @@
+import slumber
+from flask import Flask, render_template, request,redirect
+import config
+
+ENDPOINT = "https://modernizacaopublica.pulseway.com/api/v2/"
+
+try:
+    api = slumber.API(ENDPOINT, auth=(config.USERNAME, config.PASSWORD))
+    # use limit & offset parameters
+    result = api.notifications.get(limit='100', offset='0')
+    # if result['message'].startswith('')
+    update = []
+    critical = []
+    #print(result['data'])
+    newdict = result['data']
+    for i in newdict:
+
+        if i['priority'] == 'low' or 'normal':
+            update.append(i['message'])
+            print(i)
+        elif i['priority'] == 'elevated' or 'critical':
+            critical.append(i['message'])
+
+except Exception as e:
+    print('GetNotifications raised an exception.')
+
+app = Flask(__name__)
+
+@app.route('/notifications')
+def notifications():
+    return render_template('pending_reboot.html', update=update)
+
+@app.route('/critical')
+def critical_notifications():
+    return render_template('critical.html', update= critical)
+
+@app.route('/remove_item', methods=['POST'])
+def delete_notification():
+    try:
+        api = slumber.API(ENDPOINT, auth=(config.USERNAME, config.PASSWORD))
+        notification_id = request.form['notification_id']
+        result = api.notifications(notification_id).delete()
+        print(result)
+    except Exception as e:
+        print('DeleteNotification raised an exception.')
+
+    # Redirect back to the original page
+    return redirect(request.referrer)
+
+app.run("0.0.0.0",8130,True)
