f9728b379390e23154e8b66da0629fd7da10b8fb
Lucas Fernandes <lucasmatdlx@hotmail.com>
2023-10-17 00:13:10 +0000

    Update test.py

--- test.py
+++ test.py
@@ -4,35 +4,55 @@ import config
 
 ENDPOINT = "https://modernizacaopublica.pulseway.com/api/v2/"
 
-try:
-    api = slumber.API(ENDPOINT, auth=(config.USERNAME, config.PASSWORD))
-    # use limit & offset parameters
-    result = api.notifications.get(limit='100', offset='0')
-    # if result['message'].startswith('')
-    update = []
-    critical = []
-    #print(result['data'])
-    newdict = result['data']
-    for i in newdict:
+def get_api_results():
+    try:
+        api = slumber.API(ENDPOINT, auth=(config.USERNAME, config.PASSWORD))
+        # use limit & offset parameters
+        result = api.notifications.get(limit='100', offset='0')
+        # if result['message'].startswith('')
+        update = []
+        critical = []
+        #print(result['data'])
+        newdict = result['data']
+        for i in newdict:
 
-        if i['priority'] == 'low' or 'normal':
-            update.append(i)
-            print(i)
-        elif i['priority'] == 'elevated' or 'critical':
-            critical.append(i)
+            if i['priority'] == 'low' or 'normal':
+                update.append(i)
+                print(i)
+        #    elif i['priority'] == 'elevated' or 'critical':
+        #        critical.append(i)
 
-except Exception as e:
-    print('GetNotifications raised an exception.')
+        return update
+    except Exception as e:
+        print('GetNotifications raised an exception.')
+
+def get_critical_results():
+    try:
+        api = slumber.API(ENDPOINT, auth=(config.USERNAME, config.PASSWORD))
+        # use limit & offset parameters
+        result = api.notifications.get(limit='100', offset='0')
+        critical = []
+        newdict = result['data']
+        for i in newdict:
+
+            if i['priority'] == 'elevated' or 'critical':
+                critical.append(i)
+                print(i)
+
+        return critical
+    except Exception as e:
+        print('GetNotifications raised an exception.')
+
 
 app = Flask(__name__)
 
 @app.route('/notifications')
 def notifications():
-    return render_template('pending_reboot.html', update=update)
+    return render_template('pending_reboot.html', update=get_api_results())
 
 @app.route('/critical')
 def critical_notifications():
-    return render_template('critical.html', update= critical)
+    return render_template('critical.html', update=get_critical_results())
 
 @app.route('/remove_item', methods=['POST'])
 def delete_notification():
